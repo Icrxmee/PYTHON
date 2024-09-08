7ed2a64be14b3b6c892f2882d898e5019990a1a0
Pedro Ícaro <160777218+Icrxmee@users.noreply.github.com>
2024-09-07 22:01:24 -0300

    Add files via upload

---
+++ operadores_aritméticos.py
@@ -0,0 +1,37 @@
+# ADIÇÃO DE NÚMEROS
+print ('Vamos somar dois números.')
+
+n1 = int ( input('Escreva um valor:'))
+n2 = int ( input('Escrava outro valor:'))
+rs1 = n1 + n2
+
+print ('O Valor da soma entre o número {} e o número {} será: {}' .format(n1, n2, rs1))
+
+# SUBTRAÇÃO DE NÚMEROS
+print ('Vamos subtrair dois números')
+
+n3 = int ( input('Escreva um valor:'))
+n4 = int ( input('Escreva outro valor:'))
+rs2 = n3 - n4
+
+print  ('o Valor da subtração entre  o número {} e o número {} será: {}' .format(n3, n4, rs2))
+
+
+# MULTIPLICAÇÃO DE NÚMEROS 
+print ('Vamos multiplicar dois números')
+
+n5 = int ( input('Escreva um valor:'))
+n6 = int ( input('Escreva outro valor'))
+rs3 =  n5 * n6
+
+print ('O Valor da multiplicação entre o número {} e o número {} será: {}' .format(n5, n6, rs3))
+
+
+# DIVISÃO DE NÚMEROS
+print ('Vamos dividir dois números')
+
+n7 = int (input ('Escreva um valor:'))
+n8 = int (input ('Digite outro valor'))
+rs4 = n7 / n8 
+
+print (' O Valor  da divisão entre  número {} e o número {} será: {}' .format(n7,  n8, rs4))
